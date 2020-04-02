d01a715353f5752e2b355b3a26e6ce062b3b07c3
Michael Chen <mzchen@udel.edu>
2020-04-02 15:55:53 -0400

    Working on Shiny App

---
+++ v1/app.R
@@ -0,0 +1,102 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+
+    ui <- navbarPage(
+        "Innovation Rates and College Characteristics",
+        tabPanel("Plot",
+             # Application title
+             titlePanel("Relationship between Admissions Rates and Inventors"),
+             
+             # Show a plot of the generated distribution
+             mainPanel(
+                 plotOutput("plot")
+             )
+         ),
+        tabPanel("Model",
+                 fluidPage(
+                     titlePanel("Model Title"),
+                     sidebarLayout(
+                         sidebarPanel(
+                             selectInput(
+                                 "plot_type",
+                                 "Plot Type",
+                                 c("Option A" = "a", "Option B" = "b")
+                             )),
+                         mainPanel(plotOutput("line_plot")))
+                 )),
+        tabPanel("Discussion",
+                 titlePanel("Discussion Title"),
+                 p("Tour of the modeling choices you made and 
+              an explanation of why you made them")),
+        tabPanel("About", 
+                 titlePanel("About This Project"),
+                 h3("Project Background and Motivations"),
+                 p("The goal of this project is to conduct a data-driven analysis on 
+                   the relationship between innovation rates by college (obtained from 
+                   Raj Chetty's Opportunity Insights) and institution level data obtained 
+                   from College Scorecard. With so much scrutiny going into whether college 
+                   is really worth it or not, the key factor that most researchers have 
+                   been examining is the earnings of students right out of college - this 
+                   is the most common barometer of success. However, having read Andrew Yang's
+                   book 'Smart People Should Build Things', my main concern with the 'outcome'
+                   of college is whether people are creating something of value - I define value
+                   as higher innovation rates (which is measured through number of patents registered).
+                   Throughout the project, I will run regressions on various data for innovation 
+                   rates (total number of patents granted to sstudents, share of inventors among 
+                   students, total number of patent citations obtained by students, etc.) and 
+                   various college characteristics (percentage of high income students, average 
+                   SAT/ACT score, percentage of degrees awarded in various fields)."),
+                 h3("Dataset 1 Used"),
+                 p("The first data source comes from Raj Chetty's Opportunity Insights data, which harnesses 
+                   the power of big data to create scalable policy solutions to restore the American Dream. 
+                   In the data source, the table presents estimates of students' patent rates by the college
+                   they attended, where college is what place each child attends as the institution the child
+                   attended for the greatest amount of time during the 4 calendar years in which the child 
+                   turned 19-22. The most important variable is called 'inventor' and represents the share of
+                   inventors among students at each college. For more information about the data source and 
+                   other data on upward mobility, race and economic opportunity in the US, impact of neighborhoods,
+                   and impacts of teacher, please visit this link: https://opportunityinsights.org/data/"),
+                 h3("Dataset 2 Used"),
+                 p("The other dataset I use is from the College Scorecard project - this is designed to increase 
+                   transparency and provide data to help students and families compare college costs and outcomes 
+                   as they weigh the tradeoffs of different colleges. The data I choose to focus on are data files
+                   with data about institutions as a whole and include variables that map each college's name, 
+                   location, degree type, instituion revenue, academic areas offered, admissions rates, 
+                   SAT/ACT scores, and much more. The goal is to compare this dataset with innovation rates 
+                   measured at each college and see if there are correlations between characteristics of each 
+                   college and innovation rates. For more information about the College Scorecard project and 
+                   its data sources, please visit this link: https://collegescorecard.ed.gov/data/"),
+                 h3("About Me"),
+                 p("My name is Michael Chen and I'm a current sophomore at Harvard studying Applied Math and 
+                 Economics with a secondary in Government. As a co-founder of a biotech startup, I'm quite
+                 intrigued by the innovation rates at colleges and how to increase this 'builder'
+                 mentality among our students. My github repo for this project can be accessed at this link:
+                 https://github.com/michaelzchen/finalproject.git. You can reach me at: chen_michael@college.harvard.edu 
+                 or on Linkedin: https://www.linkedin.com/in/michael-c-134086135/")))
+
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    # Send a pre-rendered image, and don't delete the image after sending it
+    output$plot <- renderImage({
+        # When input$n is 3, filename is ./images/image3.jpeg
+        filename <- normalizePath(file.path('Reg_Adm_Rates_Inventors.png'))
+        
+        # Return a list containing the filename and alt text
+        list(src = filename, width = 700, length = 800)
+        
+    }, deleteFile = FALSE)
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
